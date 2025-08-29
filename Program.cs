2043e5d0089bd1a75de9a1fc7c14a97c6d65509f
vmol <vmol@itu.dk>
2025-08-29 11:42:17 +0200

    Update Program.cs

--- Program.cs
+++ Program.cs
@@ -1,4 +1,4 @@
-﻿List<string> cheeps = new() {"Hello, BDSA students!", "Welcome to the course!", "I hope you had a good summer"}
+﻿List<string> cheeps = new() { "Hello, BDSA students!", "Welcome to the course!", "I hope you had a good summer" };
 
 foreach (var cheep in cheeps)
 {
