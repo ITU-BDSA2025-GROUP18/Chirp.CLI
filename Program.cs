bcdc98f445a2c693b0e4268bdfc8479ee1b51787
Casper <cmje@itu.dk>
2025-09-03 14:24:46 +0200

    refactor of program, created new UserInterface class
    Added class functionality to Program.
    
    Refactored code so it works in class format.
    
    Created a new UserInterface class (no code inside yet)

--- Program.cs
+++ Program.cs
@@ -3,61 +3,65 @@ using System.IO;
 using System.Runtime.Serialization;
 using System.Text;
 
-// Læser alle beskeder fra chirp_cli_db.csv-filen. Bemærk dag/måned er omvendt af Eduards på GitHub...
-void read()
+public class Program
 {
-    string filepath = "chirp_cli_db.csv";
-
-    StreamReader reader = new StreamReader(filepath);
-
-    reader.ReadLine(); //Skipper første linje i filen: "Author,Message,Timestamp", da denne ikke er en besked
-
-    while (!reader.EndOfStream)
+    public static void main(string[] args)
     {
-        string[] line = reader.ReadLine()!.Split(",");
+        if (args.Length< 1) Console.WriteLine("Invalid argument(s)");
+        if (args[0] == "read") read();
+        else if (args[0] == "cheep") cheep();
+    }
+    // Læser alle beskeder fra chirp_cli_db.csv-filen. Bemærk dag/måned er omvendt af Eduards på GitHub...
+    void read()
+    {
+        string filepath = "chirp_cli_db.csv";
 
-        string author = line[0];
+        StreamReader reader = new StreamReader(filepath);
 
-        StringBuilder sb = new StringBuilder();
-        for (int i = 1; i < line.Length - 1; i++)
+        reader.ReadLine(); //Skipper første linje i filen: "Author,Message,Timestamp", da denne ikke er en besked
+
+        while (!reader.EndOfStream)
         {
-            sb.Append(line[i]);
-            if (i < line.Length - 2) sb.Append(",");
+            string[] line = reader.ReadLine()!.Split(",");
+
+            string author = line[0];
+
+            StringBuilder sb = new StringBuilder();
+            for (int i = 1; i < line.Length - 1; i++)
+            {
+                sb.Append(line[i]);
+                if (i < line.Length - 2) sb.Append(",");
+            }
+
+            sb.Replace("\"", ""); //Formatterer beskeden rigtigt
+            string message = sb.ToString();
+
+            long unixTimestamp = long.Parse(line[line.Length - 1]);
+            string timestamp = DateTimeOffset.FromUnixTimeSeconds(unixTimestamp).LocalDateTime.ToString();
+
+            Console.WriteLine(author + " @ " + timestamp + ": " + message);
+
+            Thread.Sleep(1000);
+        }
+    }
+
+    //Work in progress. Skal kunne tilføje en besked til chirp_cli_db.csv med user og tidspunkt korrekt angivet
+    void cheep()
+    {
+        if (args.Length < 2)
+        {
+            Console.WriteLine("Wrong syntax -- \"cheep\"-argument needs to be followed by a message");
+            return;
         }
 
-        sb.Replace("\"", ""); //Formatterer beskeden rigtigt
-        string message = sb.ToString();
+        string author = Environment.UserName;
+        string message = args[1];
+        string utcTimestamp = DateTimeOffset.UtcNow.ToUnixTimeSeconds().ToString();
 
-        long unixTimestamp = long.Parse(line[line.Length - 1]);
-        string timestamp = DateTimeOffset.FromUnixTimeSeconds(unixTimestamp).LocalDateTime.ToString();
+        string line = author + ",\"" + message + "\"," + utcTimestamp;
 
-        Console.WriteLine(author + " @ " + timestamp + ": " + message);
-
-        Thread.Sleep(1000);
+        StreamWriter sw = File.AppendText("chirp_cli_db.csv");
+        sw.WriteLine(line);
+        sw.Close();
     }
 }
-
-//Work in progress. Skal kunne tilføje en besked til chirp_cli_db.csv med user og tidspunkt korrekt angivet
-void cheep()
-{
-    if (args.Length < 2)
-    {
-        Console.WriteLine("Wrong syntax -- \"cheep\"-argument needs to be followed by a message");
-        return;
-    }
-
-    string author = Environment.UserName;
-    string message = args[1];
-    string utcTimestamp = DateTimeOffset.UtcNow.ToUnixTimeSeconds().ToString();
-
-    string line = author + ",\"" + message + "\"," + utcTimestamp;
-
-    StreamWriter sw = File.AppendText("chirp_cli_db.csv");
-    sw.WriteLine(line);
-    sw.Close();
-}
-
-//Koden nedenfor er basically vores main
-if (args.Length < 1) Console.WriteLine("Invalid argument(s)");
-if (args[0] == "read") read();
-else if (args[0] == "cheep") cheep();
