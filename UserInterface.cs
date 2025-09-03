5f7585f993c7f2944a764a41a11c9e2fea258cf7
Casper <cmje@itu.dk>
2025-09-03 14:47:22 +0200

    creating function that prints all cheeps via UserInterface
    need a new class called cheep that contains cheeps.

--- UserInterface.cs
+++ UserInterface.cs
@@ -1,9 +1,4 @@
 public class UserInterface : Program
 {
-    public static void Main(string[] args)
-    {
-        if (args.Length< 1) Console.WriteLine("Invalid argument(s)");
-        if (args[0] == "read") read();
-        else if (args[0] == "cheep") cheep(args);
-    }
+    void PrintCheeps(Enumerable<cheep> cheeps)
 }
