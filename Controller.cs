a2c70500e2c5dff306cd1b6e7a47d85bff514aa0
MillrDev <millervinjr@proton.me>
2025-09-08 17:36:35 +0200

    Controller class implemented
    In order to get a cleaner main method in Program.cs

--- Controller.cs
+++ Controller.cs
@@ -1,6 +1,47 @@
+using System.CommandLine;
+
 namespace Chirp.CLI;
+
+using SimpleDB;
 
 public class Controller
 {
-    
+    private readonly CSVDatabase<Cheep<string>> _database;
+
+    public Controller(CSVDatabase<Cheep<string>> database)
+    {
+        _database = database;
+    }
+
+    public int Run(string[] args)
+    {
+        var author = Environment.UserName;
+        var utcTimestamp = DateTimeOffset.UtcNow.ToUnixTimeSeconds();
+
+        var rootCommand = new RootCommand("Chirp command line interface");
+        var readCommand = new Command("read", "Read messages in the database");
+        rootCommand.Add(readCommand);
+
+        var cheepCommand = new Command("cheep", "Send a message to the database");
+        var cheepArg = new Argument<string>("Message that'll be sent to the database");
+        cheepCommand.Add(cheepArg);
+        rootCommand.Add(cheepCommand);
+
+        var parseResult = rootCommand.Parse(args);
+        if (parseResult.Errors.Count > 0)
+        {
+            foreach (var parseError in parseResult.Errors)
+            {
+                Console.Error.WriteLine(parseError.Message);
+            }
+
+            return 1;
+        }
+
+        if (parseResult.GetResult(readCommand) != null) UserInterface.PrintCheeps(_database.Read(20));
+        if (parseResult.GetResult(cheepCommand)?.GetValue(cheepArg) is { } message)
+            _database.Store(new Cheep<string>(author, message, utcTimestamp));
+
+        return 0;
+    }
 }
--- Program.cs
+++ Program.cs
@@ -1,5 +1,6 @@
 ﻿using System.Globalization;
 using System.CommandLine;
+using Chirp.CLI;
 using CsvHelper;
 using SimpleDB;
 
@@ -9,34 +10,7 @@ public class Program
     {
         var dataPath = "chirp_cli_db.csv";
         var database = new CSVDatabase<Cheep<string>>(dataPath);
-
-        var author = Environment.UserName;
-        var utcTimestamp = DateTimeOffset.UtcNow.ToUnixTimeSeconds();
-
-        var rootCommand = new RootCommand("Chirp command line interface");
-        var readCommand = new Command("read", "Read messages in the database");
-        rootCommand.Add(readCommand);
-
-        var cheepCommand = new Command("cheep", "Send a message to the database");
-        var cheepArg = new Argument<string>("Message that'll be sent to the database");
-        cheepCommand.Add(cheepArg);
-        rootCommand.Add(cheepCommand);
-
-        var parseResult = rootCommand.Parse(args);
-        if (parseResult.Errors.Count > 0)
-        {
-            foreach (var parseError in parseResult.Errors)
-            {
-                Console.Error.WriteLine(parseError.Message);
-            }
-
-            return 1;
-        }
-
-        if (parseResult.GetResult(readCommand) != null) UserInterface.PrintCheeps(database.Read(20));
-        if (parseResult.GetResult(cheepCommand)?.GetValue(cheepArg) is { } message)
-            database.Store(new Cheep<string>(author, message, utcTimestamp));
-
-        return 0;
+        var controller = new Controller(database);
+        return controller.Run(args);
     }
 }
