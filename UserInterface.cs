dea413bd150da35776b798169430506e3100fe71
MillrDev <millervinjr@proton.me>
2025-09-08 17:13:36 +0200

    Refactor UserInterface class to only write records
    Co-Authored-By: EranZejnelovski <228920027+EranZejnelovski@users.noreply.github.com>
    Co-Authored-By: Oliver Flinck Sheye <189235422+osheITU@users.noreply.github.com>

--- UserInterface.cs
+++ UserInterface.cs
@@ -1,34 +1,26 @@
 using System.Globalization;
-using Chirp.CLI;
 using CsvHelper;
+using SimpleDB;
 
 public class UserInterface : Program
 {
-    //Function that gets all cheeps from the crip_cli_db.csv file
-    private static IEnumerable<Cheep> ReadCheeps()
-    {
-        using var reader = new StreamReader("chirp_cli_db.csv");
-        using var csv = new CsvReader(reader, CultureInfo.InvariantCulture);
-        return csv.GetRecords<Cheep>().ToList();
-    }
-
     //Function that writes out all cheeps in console
-    public static void PrintCheeps()
+    public static void PrintCheeps(IEnumerable<SimpleDB.Cheep<string>> cheeps)
     {
-        foreach (var cheep in ReadCheeps())
+        foreach (var cheep in cheeps)
         {
             WriteRecordToConsole(cheep);
         }
     }
-    
+
     //Function that writes out a cheep in console.
-    private static void WriteRecordToConsole(Cheep record)
+    private static void WriteRecordToConsole(SimpleDB.Cheep<string> record)
     {
         var formattedTimeStamp = DateTimeOffset
             .FromUnixTimeSeconds(record.Timestamp)
             .LocalDateTime
             .ToString(CultureInfo.InvariantCulture);
 
-        Console.WriteLine($"{record.Author} @ {formattedTimeStamp}: {record.Message}");  
+        Console.WriteLine($"{record.Author} @ {formattedTimeStamp}: {record.Message}");
     }
 }
