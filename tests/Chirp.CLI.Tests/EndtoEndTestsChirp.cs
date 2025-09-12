dd0242c5ecf5817d1a3b3050438c602fab50d07a
Casper <Casper.meier.jenkins@gmail.com>
2025-09-12 11:19:26 +0200

    Create EndtoEndTestsChirp.cs
    EndtoEnd test, reads ten cheeps

---
+++ tests/Chirp.CLI.Tests/EndtoEndTestsChirp.cs
@@ -0,0 +1,15 @@
+﻿namespace Chirp.CLI.Tests;
+
+public class EndtoEndTestsChirp
+{
+    [Fact]
+    public void TestReadTenCheeps()
+    {
+        // Arrange
+        var args = new string[] { "read", "10" };
+        // Act
+        var result = Program.Main(args);
+        // Assert
+        Assert.Equal(0, result);
+    }
+}
