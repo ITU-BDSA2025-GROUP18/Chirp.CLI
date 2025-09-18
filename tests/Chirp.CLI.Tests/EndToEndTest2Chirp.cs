d145082e4e65fd7d538b59e5fe4400ebc7706510
erze <228920027+EranZejnelovski@users.noreply.github.com>
2025-09-18 21:42:51 +0200

    Setup for ChirpStoreTest
    Co-Authored-By: Casper <225747490+shtaroo@users.noreply.github.com>

--- tests/Chirp.CLI.Tests/EndToEndTest2Chirp.cs
+++ tests/Chirp.CLI.Tests/EndToEndTest2Chirp.cs
@@ -1,4 +1,6 @@
-﻿namespace Chirp.CLI.Tests;
+﻿using SimpleDB;
+
+namespace Chirp.CLI.Tests;
 
 public class EndToEndTest2Chirp : IDisposable
 {
@@ -18,5 +20,28 @@ public class EndToEndTest2Chirp : IDisposable
     {
         // Makes sure the CSV-file starts with the correct headers. (Thanks oli)
         File.WriteAllText(_dbPath, "Author,Message,Timestamp\n");
+    }
+
+    public void DoesChirpStoreDataCorrectly()
+    /*This will be a test where we check if data is stored correctly into the database.
+    We will test the following message: "Hello!!!" will be stored in the database together with
+    its author and timestamp. 
+    We'll therefore create a new CSVDatabase that points to our temporary file &
+    create a controller that uses our new CSVDatabase  */
+    {
+        var db = new CSVDatabase<Cheep<T>>(_dbPath);
+        var controller = new Controller(db);
+
+        controller.Run(new[] { "cheep", "Hello!!!" }); /*"cheep" = our keyword for our parser to read
+        "Hello!!!" = message we want to cheep*/
+        
+        //Assertion
+        /*First we check that we have our cheep stored. (2 lines because first line
+        is our header from earlier*/
+        var lines = File.ReadAllLines(_dbPath);
+        Assert.Equal(2, lines.Length);
+        
+        //now what am i missing
+        Assert.Contains("Hello!!!", x);
     }
 }
