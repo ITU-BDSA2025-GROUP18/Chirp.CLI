0b946faee9c95c5682e244b3d0d50be98681e386
erze <228920027+EranZejnelovski@users.noreply.github.com>
2025-09-18 21:12:53 +0200

    Creation of temporary CSV-File
    Creation of temporary CSV-File
    
    Co-Authored-By: Casper <225747490+shtaroo@users.noreply.github.com>

---
+++ tests/Chirp.CLI.Tests/EndToEndTest2Chirp.cs
@@ -0,0 +1,19 @@
+﻿namespace Chirp.CLI.Tests;
+
+public class EndToEndTest2Chirp : IDisposable
+{
+    private readonly string _dbPath = Path.Combine(Path.GetTempPath(), Path.GetRandomFileName());
+    
+    public void Dispose()
+    {
+        if (File.Exists(_dbPath))
+        {
+            File.Delete(_dbPath);
+        }
+    }
+    
+    public ChirpEndToEndTests()
+    {
+        File.WriteAllText(_dbPath, "Author,Message,Timestamp\n");
+    }
+}
