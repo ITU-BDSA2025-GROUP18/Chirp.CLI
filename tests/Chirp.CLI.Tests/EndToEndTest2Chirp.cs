bf97250fbd6840ed0c54c86aa77b8c6b77222114
erze <228920027+EranZejnelovski@users.noreply.github.com>
2025-09-18 21:14:29 +0200

    Adding of comments
    Comments to first part

--- tests/Chirp.CLI.Tests/EndToEndTest2Chirp.cs
+++ tests/Chirp.CLI.Tests/EndToEndTest2Chirp.cs
@@ -2,8 +2,10 @@
 
 public class EndToEndTest2Chirp : IDisposable
 {
+    //Lets first create a temporary csv-file which we will use for this test.
     private readonly string _dbPath = Path.Combine(Path.GetTempPath(), Path.GetRandomFileName());
     
+    //But let's also ensure that a csv-file with this name doesn't currently exist.
     public void Dispose()
     {
         if (File.Exists(_dbPath))
@@ -14,6 +16,7 @@ public class EndToEndTest2Chirp : IDisposable
     
     public ChirpEndToEndTests()
     {
+        // Makes sure the CSV-file starts with the correct headers. (Thanks oli)
         File.WriteAllText(_dbPath, "Author,Message,Timestamp\n");
     }
 }
