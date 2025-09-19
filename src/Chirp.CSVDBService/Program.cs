85122655e32a4b894b7cf95b61c593aefa78664e
LAPTOP-HIBQ8KAL\sander <salc@itu.dk>
2025-09-19 10:24:21 +0200

    Added simple cheeps endpoint
    Co-Authored-By: MillrDev <92321245+MillrDev@users.noreply.github.com>
    Co-Authored-By: Casper <225747490+shtaroo@users.noreply.github.com>

--- src/Chirp.CSVDBService/Program.cs
+++ src/Chirp.CSVDBService/Program.cs
@@ -1,6 +1,6 @@
 var builder = WebApplication.CreateBuilder(args);
 var app = builder.Build();
 
-app.MapGet("/", () => "Hello World!");
+app.MapGet("/cheeps", () => new Cheep("me", "Hej!", 1684229348));
 
 app.Run();
