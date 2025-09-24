c97669506894ca0db1a13fc675502579bcf8cc5b
MillrDev <millervinjr@proton.me>
2025-09-24 13:25:41 +0200

    Updated so that CSV file is created if it doesnt exist

--- src/Chirp.CSVDBService/Controllers/CheepRepository.cs
+++ src/Chirp.CSVDBService/Controllers/CheepRepository.cs
@@ -1,15 +1,32 @@
 ﻿using System.Globalization;
+using Chirp.CSVDBService.Models;
 using CsvHelper;
 
 namespace Chirp.CSVDBService.Controllers;
 
 public class CheepRepository<T>
 {
-    const string path = "Data/chirp_cli_db.csv";
-    
+    private string _path = "Data/chirp_cli_db.csv";
+    private StreamWriter _writer;
+    private CsvWriter _csv;
+
+    public CheepRepository()
+    {
+        if (!File.Exists(_path))
+        {
+            using var writer = new StreamWriter(_path);
+            using var csv = new CsvWriter(writer, CultureInfo.InvariantCulture);
+            csv.WriteHeader<T>();
+        }
+
+        // Open the main writer for appending
+        _writer = new StreamWriter(_path, append: true);
+        _csv = new CsvWriter(_writer, CultureInfo.InvariantCulture);
+    }
+
     public IEnumerable<T> Read(int? limit = null)
     {
-        using var reader = new StreamReader(path);
+        using var reader = new StreamReader(_path);
         using var csv = new CsvReader(reader, CultureInfo.InvariantCulture);
 
         var records = csv.GetRecords<T>().ToList();
@@ -17,14 +34,14 @@ public class CheepRepository<T>
 
         if (limit == null) return records;
         if (limit < 0) throw new ArgumentException("limit must be positive");
-        if (limit > n) limit = n; 
-        
+        if (limit > n) limit = n;
+
         return records.GetRange(0, limit.Value);
     }
-    
+
     public void Store(T record)
     {
-        using var writer = new StreamWriter(path, append: true);
+        using var writer = new StreamWriter(_path, append: true);
         using var csv = new CsvWriter(writer, CultureInfo.InvariantCulture);
         csv.WriteRecord(record);
         writer.WriteLine();
