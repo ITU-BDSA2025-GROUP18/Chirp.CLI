ee0066292efcacaee3da19825152c81dab5d831e
oshe <oshe@itu.dk>
2025-09-26 13:38:54 +0200

    Added to DB connection to DBFacade
    Co-Authored-By: Sander <73191374+SanderLahoz@users.noreply.github.com>
    Co-Authored-By: Casper <225747490+shtaroo@users.noreply.github.com>

--- src/Chirp.Razor/DBFacade.cs
+++ src/Chirp.Razor/DBFacade.cs
@@ -1,6 +1,18 @@
-﻿namespace Chirp.Razor;
+﻿using Microsoft.Data.Sqlite;
+
+namespace Chirp.Razor;
 
 public class DBFacade
 {
-    
+    private const string sqlDBFilePath = "./Data/database.db";
+    private readonly SqliteConnection connection;
+
+    public DBFacade()
+    {
+        connection = new SqliteConnection($"Data Source={sqlDBFilePath}");
+        connection.Open();
+    }
+
+    public void CloseConnection() { connection.Close(); }
+
 }
