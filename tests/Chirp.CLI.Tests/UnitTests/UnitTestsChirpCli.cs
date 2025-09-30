90a38ebafb444059e398f54fb2381645c75725c7
MillrDev <vmol@itu.dk>
2025-09-30 18:05:56 +0200

    added CheepsShouldSerializeAndDeserialize test

--- tests/Chirp.CLI.Tests/UnitTests/UnitTestsChirpCli.cs
+++ tests/Chirp.CLI.Tests/UnitTests/UnitTestsChirpCli.cs
@@ -1,10 +1,23 @@
-﻿namespace Chirp.CLI.Tests;
+﻿using System.Text.Json;
+
+namespace Chirp.CLI.Tests;
 
 public class UnitTestsChirpCli
 {
+
+    // This test simulated what happens when cheep is uploaded to the web server
     [Fact]
-    public void Test1()
+    public void CheepsShouldSerializeAndDeserialize()
     {
+        // Arrange
+        var cheep = new Cheep<string>("Alice", "Lorem ipsum", 12345);
+
+        // Act
+        var json = JsonSerializer.Serialize(cheep);
+        var jsonDeserialized = JsonSerializer.Deserialize<Cheep<string>>(json);
+
+        // Assert
+        Assert.Equal(cheep, jsonDeserialized);
     }
 
     // Test
