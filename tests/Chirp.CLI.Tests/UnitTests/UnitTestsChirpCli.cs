ef99b31808954a0e6272f82e97a3dd27fb9e8fe4
MillrDev <vmol@itu.dk>
2025-09-30 18:17:34 +0200

    Added  CheepsShouldBePrintedInTheConsole()

--- tests/Chirp.CLI.Tests/UnitTests/UnitTestsChirpCli.cs
+++ tests/Chirp.CLI.Tests/UnitTests/UnitTestsChirpCli.cs
@@ -1,10 +1,10 @@
-﻿using System.Text.Json;
+﻿using System.CommandLine;
+using System.Text.Json;
 
 namespace Chirp.CLI.Tests;
 
 public class UnitTestsChirpCli
 {
-
     // This test simulated what happens when cheep is uploaded to the web server
     [Fact]
     public void CheepsShouldSerializeAndDeserialize()
@@ -20,5 +20,24 @@ public class UnitTestsChirpCli
         Assert.Equal(cheep, jsonDeserialized);
     }
 
-    // Test
+    // This test makes sure that the UserInterface static method PrintCheeps correctly prints cheeps to the console
+    [Fact]
+    public void CheepsShouldBePrintedInTheConsole()
+    {
+        // Arrange
+        var cheeps = new List<Cheep<string>>
+        {
+            new Cheep<string>("Alice", "Lorem ipsum lorem ipsum testing testing", 12345)
+        };
+
+        using var sw = new StringWriter(); // Fake console
+        Console.SetOut(sw); // Routes stuff to the fake console
+
+        // Act
+        UserInterface<Cheep<string>>.PrintCheeps(cheeps);
+
+        // Assert
+        var output = sw.ToString();
+        Assert.Contains("Lorem ipsum lorem ipsum testing testing", output); // Fake console should contain
+    }
 }
