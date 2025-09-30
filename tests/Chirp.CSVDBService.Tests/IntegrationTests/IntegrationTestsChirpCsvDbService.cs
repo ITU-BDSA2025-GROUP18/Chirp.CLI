8d254bdb74c0fe324ce5bc267292da410668f74d
MillrDev <vmol@itu.dk>
2025-09-30 19:55:26 +0200

    Implemented StoreShouldStoreACheepInTheCsvdb() test

--- tests/Chirp.CSVDBService.Tests/IntegrationTests/IntegrationTestsChirpCsvDbService.cs
+++ tests/Chirp.CSVDBService.Tests/IntegrationTests/IntegrationTestsChirpCsvDbService.cs
@@ -1,6 +1,29 @@
-﻿namespace Chirp.CSVDBService.Tests.IntegrationTests;
+﻿using System.Globalization;
+using Chirp.CSVDBService.Controllers;
+using Chirp.CSVDBService.Models;
+using CsvHelper;
+
+namespace Chirp.CSVDBService.Tests.IntegrationTests;
 
 public class IntegrationTestsChirpCsvDbService
 {
-    
+    [Fact]
+    private void StoreShouldStoreACheepInTheCsvdb()
+    {
+        // Arrange
+        var controller = new CheepRepository<Cheep<string>>();
+        var cheep = new Cheep<string>("Alice", "Lorem ipsum", 12345);
+
+        var home = Environment.GetFolderPath(Environment.SpecialFolder.UserProfile);
+        var path = Path.Combine(home, "chirp_cli_db.csv");
+
+        // Act
+        controller.Store(cheep);
+        using var reader = new StreamReader(path);
+        using var csv = new CsvReader(reader, CultureInfo.InvariantCulture);
+        var records = csv.GetRecords<Cheep<string>>().ToList();
+
+        // Assert
+        Assert.Contains(cheep, records);
+    }
 }
