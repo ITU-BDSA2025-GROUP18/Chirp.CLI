4afbc349eb0e6cd3f9043831777b5bdd1398abba
oshe <oshe@itu.dk>
2025-10-07 17:58:56 +0200

    Database seeding
    This completes 'Software Development point 1d) Seeding the database' of this week's tasks

--- src/Chirp.Razor/ChirpDBContext.cs
+++ src/Chirp.Razor/ChirpDBContext.cs
@@ -31,5 +31,9 @@ public class ChirpDBContext : DbContext
     public DbSet<Cheep> Cheeps { get; set; }
     public DbSet<Author> Authors { get; set; }
 
-    public ChirpDBContext(DbContextOptions<ChirpDBContext> options) : base(options) { }
+    public ChirpDBContext(DbContextOptions<ChirpDBContext> options) : base(options)
+    {
+        // Seeding the database with example data
+        DbInitializer.SeedDatabase(this);
+    }
 }
