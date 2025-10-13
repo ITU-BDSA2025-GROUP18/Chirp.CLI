57f5241a2c91aefbfea77e1d4b4ac45c2dfd7881
ITU-ahms <knahse4@knorddoc.dk>
2025-10-13 21:09:38 +0200

    test(api): add WebAppFactory scaffold (Test env)
    Co-authored-by: Eran Zejnelovski <eran.zejnel@yahoo.com>

---
+++ tests/Chirp.Razor.Tests/WebAppFactory.cs
@@ -0,0 +1,10 @@
+﻿using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Mvc.Testing;
+
+public class WebAppFactory : WebApplicationFactory<Chirp.Razor.Program>
+{
+    protected override void ConfigureWebHost(IWebHostBuilder builder)
+    {
+        builder.UseEnvironment("Test");
+    }
+}
