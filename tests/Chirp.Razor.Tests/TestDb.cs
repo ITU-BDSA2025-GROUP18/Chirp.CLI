4fe568dac875bc7be4dab624bb5266daea24bab6
ITU-ahms <knahse4@knorddoc.dk>
2025-10-13 21:09:31 +0200

    test(db): seed messages for Helge and Adrian
    Co-authored-by: Eran Zejnelovski <eran.zejnel@yahoo.com>

--- tests/Chirp.Razor.Tests/TestDb.cs
+++ tests/Chirp.Razor.Tests/TestDb.cs
@@ -14,6 +14,10 @@ CREATE TABLE user (
   username TEXT NOT NULL,
   email TEXT NOT NULL
 );
+;
+INSERT INTO message (message_id, author_id, text, pub_date) VALUES
+  (1,1,"Hello, BDSA students!",1690892208),
+  (2,2,"Hej, velkommen til kurset.",1690895308);
 ";
         cmd.ExecuteNonQuery();
         return path;
