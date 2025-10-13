2d85096a3dfb6790bdee3f71ac8bb4617553c076
ITU-ahms <knahse4@knorddoc.dk>
2025-10-13 21:09:43 +0200

    test(api): finalize WebAppFactory configuration
    Co-authored-by: Eran Zejnelovski <eran.zejnel@yahoo.com>

--- tests/Chirp.Razor.Tests/WebAppFactory.cs
+++ tests/Chirp.Razor.Tests/WebAppFactory.cs
@@ -1,4 +1,4 @@
-﻿using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Mvc.Testing;
 
 public class WebAppFactory : WebApplicationFactory<Chirp.Razor.Program>
@@ -6,5 +6,6 @@ public class WebAppFactory : WebApplicationFactory<Chirp.Razor.Program>
     protected override void ConfigureWebHost(IWebHostBuilder builder)
     {
         builder.UseEnvironment("Test");
+        // DB peges via CHIRPDBPATH i testene.
     }
 }
