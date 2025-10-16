03446e088de56ebb0a7edce1a8f50b878e40de28
erze <228920027+EranZejnelovski@users.noreply.github.com>
2025-10-16 18:07:25 +0200

    A template for CheepCommandRepository

---
+++ src/Chirp.Razor/CheepCommandRepository.cs
@@ -0,0 +1,26 @@
+﻿using System.Globalization;
+using Microsoft.EntityFrameworkCore;
+
+namespace Chirp.Razor;
+
+#nullable disable
+
+public class CheepCommandRepository
+{
+    public string AuthorName;
+    public string Text;
+    public string Timestamp;
+}
+
+#nullable restore
+
+public interface ICheepCommandRepository
+{
+    public Task CommandAsync(String author, string text); //Future commands
+    public Task CommandAsync2 (String author, string text); //Future commands
+}
+
+/*public class CheepCommandRepository : ICheepCommandRepository // Commands
+{
+    //Commands here
+} */
